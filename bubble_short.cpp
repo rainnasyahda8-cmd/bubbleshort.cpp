9678992620d87777b67433942469bda1b56791a2
rainnasyahda8 <rainnasyahda8@gmail.com>
2026-03-06 15:27:05 +0700

    break

--- bubble_short.cpp
+++ bubble_short.cpp
@@ -10,4 +10,22 @@ void input()
     cin >> n;
 
     if (n <= 20)
+    {
+        break;
+    }
+    else
+    {
+        cout << "Error : maksimal input array adalah 20\n";
+    }
+
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "masukan elemen array" << endl;
+    cout << "====================" << endl;
+
+    for (int i = 0; i < n; i++) // i++ / i = i + 1
+    {
+        cout << "masukan nilai ke - " << i + 1 << " : ";\
+        cin >> a[i];
+    }
 }
